22559a1d169cf3ce093402cb638dea8f1760348e
Nikappa57 <lorenz.gaudino@gmail.com>
2022-12-04 01:24:59 +0100

    Simple TextFiled added

--- lib/widgets/readme_editor/readme_editor.dart
+++ lib/widgets/readme_editor/readme_editor.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
 class ReadMeEditor extends StatefulWidget {
-  ReadMeEditor(this.text);
+  ReadMeEditor(this.text, this.onChange);
   final String text;
+  final void Function(String value) onChange;
   @override
   State<ReadMeEditor> createState() => _ReadMeEditorState();
 }
 
 class _ReadMeEditorState extends State<ReadMeEditor> {
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.text = widget.text;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Scrollbar(
+          child: TextField(
+        controller: _controller,
+        expands: true,
+        maxLines: null,
+        onChanged: widget.onChange,
+      )),
+    );
   }
 }
