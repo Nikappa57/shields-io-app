383631857d86e9acb2ba11f3598967515e192f53
Nikappa57 <lorenz.gaudino@gmail.com>
2022-12-10 02:54:35 +0100

    provider registered in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:readme_editor/provider/shield.dart';
 
 import 'package:readme_editor/screens/auth_screen.dart';
 import 'package:readme_editor/screens/home.dart';
@@ -18,45 +20,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: _initialization,
-      builder: (BuildContext context, AsyncSnapshot snapshot) => MaterialApp(
-        title: 'ReadMe Editor',
-        theme: ThemeData(
-            primaryColor: Colors.purple[900],
-            backgroundColor: Colors.purple[900],
-            accentColor: Colors.orange[400],
-            accentColorBrightness: Brightness.dark,
-            elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+      builder: (BuildContext context, AsyncSnapshot snapshot) =>
+          ChangeNotifierProvider(
+        create: (ctx) => Shields(),
+        child: MaterialApp(
+          title: 'ReadMe Editor',
+          theme: ThemeData(
+              primaryColor: Colors.purple[900],
+              backgroundColor: Colors.purple[900],
+              accentColor: Colors.orange[400],
+              accentColorBrightness: Brightness.dark,
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.purple[900]),
+                ),
+              ),
+              textButtonTheme: TextButtonThemeData(
+                style: ButtonStyle(
+                  textStyle: MaterialStateProperty.all(
+                    TextStyle(color: Colors.purple[900]),
                   ),
                 ),
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.purple[900]),
-              ),
-            ),
-            textButtonTheme: TextButtonThemeData(
-              style: ButtonStyle(
-                textStyle: MaterialStateProperty.all(
-                  TextStyle(color: Colors.purple[900]),
+              )),
+          home: snapshot.connectionState != ConnectionState.done
+              ? SplashScreen()
+              : StreamBuilder(
+                  stream: FirebaseAuth.instance.authStateChanges(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return SplashScreen();
+                    }
+
+                    if (snapshot.hasData) {
+                      return FutureBuilder(
+                          future: Provider.of<Shields>(context, listen: false)
+                              .setData(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) return SplashScreen();
+                            return Home();
+                          });
+                    }
+                    return AuthScreen();
+                  },
                 ),
-              ),
-            )),
-        home: snapshot.connectionState != ConnectionState.done
-            ? SplashScreen()
-            : StreamBuilder(
-                stream: FirebaseAuth.instance.authStateChanges(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return SplashScreen();
-                  }
-                  if (snapshot.hasData) {
-                    return Home();
-                  }
-                  return AuthScreen();
-                },
-              ),
+        ),
       ),
     );
   }
